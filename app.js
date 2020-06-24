61ecf73e55241b2fb1244d36f128939a9de7a7c8
sarii0628 <9mm.p.bull@gmail.com>
2020-06-24 11:50:39 +0900

    コピペしやすいように変更

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const clipboardy = require('clipboardy');
+// const clipboardy = require('clipboardy');
 
 app.use(express.static('public'));
 
